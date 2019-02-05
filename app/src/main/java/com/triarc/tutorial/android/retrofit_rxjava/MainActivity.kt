1c816b59a7a6503e75e6c1708aca765daa77304a
devanshuverma <devanshuverma@gmail.com>
2019-02-05 11:04:56 +1100

    Create src base

---
+++ app/src/main/java/com/triarc/tutorial/android/retrofit_rxjava/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.triarc.tutorial.android.retrofit_rxjava
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Devanshu Verma on 5/2/19
+ */
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
